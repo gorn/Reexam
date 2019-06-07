53833f8ea31acd26332e70f6d0482d9b462f54c4
Jakub Těšínský <j@kub.cz>
2019-06-07 10:01:40 +0200

    opravy gorn

--- frontend/src/components/JobPosts.js
+++ frontend/src/components/JobPosts.js
@@ -5,27 +5,32 @@ class JobPosts extends Component{
         render() {
             let jobs = this.props.jobs;
 
-            console.log(this.props.jobs);
-            console.log(this.props.category);
-            console.log(this.props.area);
-            console.log(this.props.match.params.area);
+//             console.log(this.props.category);
+//             console.log(this.props.area);
+//             console.log(this.props.match.params.area);
             if(jobs <= 0){
                 return  <p>Loading...</p>
+            } else {
+              let jobsFilter = jobs.filter(
+                 job => ( !(job.category === null) )  && 
+                        (job.category.namePath === this.props.category) && 
+                        (job.area.namePath === this.props.area)
+              );
+              if(jobsFilter.length <= 0){
+                  return <p>Sorry there are no jobs</p>
             }
-            let jobsFilter = jobs.filter(job => job.category.namePath === this.props.category && job.area.namePath === this.props.location);
-            if(jobsFilter.length <= 0){
-                return <p>Sorry there are no jobs</p>
-            }
+        }
         return(
             <div>
                 <Link to ={"/"}> <p>Home</p></Link>
                 <h1>Jobs</h1>
                 <h3>Job list</h3>
                 {jobs.map(el => {
-                    return <Link to={`/job/${el._id}`}>
+                    return <span><Link to={`/job/${el._id}`}>
                         <p>{el.title}</p>
-                    </Link>
+                    </Link>{el.category === null ? 'category N/A' : el.category.name}, {el.area === null ? 'area N/A' : el.area.name}</span>
                 })}
+                
             </div>
         )
     }
