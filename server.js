ee941678ae7e55997fa42310cd8aba2a6c809255
Albina Mrazova <mrazova.albina@gmail.com>
2019-07-21 15:50:21 +0200

    Hello it's a me albi

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use('/api/categories', require('./routes/api/categories'));
 if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req, res)=>{
+    app.get('/', (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
