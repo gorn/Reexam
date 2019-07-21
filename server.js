01c18ffa28d836048c235c82543e3cb8d23c3dee
Albina Mrazova <mrazova.albina@gmail.com>
2019-07-21 15:05:16 +0200

    Hello it's a me albi

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const checkJwt = require('express-jwt');
 const config = require('config');
+const path = require('path');
+
 // const cors = require('cors');
 //
 // app.use(cors);
@@ -29,14 +31,10 @@ app.all('/*', function(req, res, next) {
 const port = process.env.PORT|| 5000;
 app.use(cookieParser());
 app.use(express.json());
-const mongoString='mongodb+srv://albi123:albi123@cluster0-ihrbv.mongodb.net/test?retryWrites=true&w=majority';
-
-mongoose.connect(mongoString, function(err) {
-    if (err) {
-        throw err;
-    } else {
-        console.log(`Successfully connected to ${mongoString}`);
-    }
+//mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://albi123:albi123@cluster0-ihrbv.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true
 });
 
 app.use('/api/users', require('./routes/api/users'));
@@ -44,5 +42,12 @@ app.use('/api/jobs', require('./routes/api/jobs'));
 app.use('/api/locations', require('./routes/api/locations'));
 app.use('/api/categories', require('./routes/api/categories'));
 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Exam API running on port ${port}!`));
