b71aabd8ed7f3a35f2a2426db623ccec52489e1f
Albina Mrazova <mrazova.albina@gmail.com>
2019-08-06 17:24:51 +0200

    FE linking logs28                       Adding favorite list 3

--- frontend/src/components/Program.js
+++ frontend/src/components/Program.js
@@ -5,6 +5,7 @@ export class Program extends Component {
     render() {
         const program = this.props.match.params.id;
         console.log(this.props.program.title);
+        let programinfo = this.props.program;
         console.log(program);
         if (!program) {
             return <p>Waiting for program</p>
@@ -13,11 +14,11 @@ export class Program extends Component {
             <div>
                 <Link to ={"/"}> <p>Home</p></Link>
                 <header>
-                    <h1>Title : {program.title}</h1>
-                    <p>Description: {program.description}</p>
-                    <p>First on air: {program.first}</p>
-                    <p>Next time in your tv: {program.next}</p>
-                    <p>Tv channel that it will be on: {program.tvchannel}</p>
+                    <h1>Title : {programinfo.title}</h1>
+                    <p>Description: {programinfo.description}</p>
+                    <p>First on air: {programinfo.first}</p>
+                    <p>Next time in your tv: {programinfo.next}</p>
+                    <p>Tv channel that it will be on: {programinfo.tvchannel}</p>
                 </header>
             </div>
         )
