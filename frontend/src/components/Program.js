10721632d1a0b7b9b0ab53fcd70b13da1c869f30
Albina Mrazova <mrazova.albina@gmail.com>
2019-08-06 17:43:27 +0200

    FE linking logs31                       Adding favorite list 3

--- frontend/src/components/Program.js
+++ frontend/src/components/Program.js
@@ -4,22 +4,24 @@ import {Link} from "react-router-dom";
 export class Program extends Component {
     render() {
         const program = this.props.match.params.id;
-        console.log(this.props.program.title);
         let programinfo = this.props.program;
         console.log(programinfo);
+        console.log(props);
+
         console.log(program);
         if (!program) {
             return <p>Waiting for program</p>
+
         }
         return (
             <div>
                 <Link to ={"/"}> <p>Home</p></Link>
                 <header>
-                    <h1>Title : {this.props.program.title}</h1>
-                    <p>Description: {this.props.program.description}</p>
-                    <p>First on air: {this.props.program.first}</p>
-                    <p>Next time in your tv: {this.props.program.next}</p>
-                    <p>Tv channel that it will be on: {this.props.program.tvchannel}</p>
+                    <h1>Title : {programinfo.title}</h1>
+                    <p>Description: {programinfo.description}</p>
+                    <p>First on air: {programinfo.first}</p>
+                    <p>Next time in your tv: {programinfo.next}</p>
+                    <p>Tv channel that it will be on: {programinfo.tvchannel}</p>
                 </header>
             </div>
         )
