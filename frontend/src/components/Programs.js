24df17ee694448edadb9b99fd7ced5add50d87eb
Albina Mrazova <mrazova.albina@gmail.com>
2019-08-06 14:50:42 +0200

    FE linking logs13                       Adding favorite list 3

--- frontend/src/components/Programs.js
+++ frontend/src/components/Programs.js
@@ -3,34 +3,41 @@ import {Link} from "react-router-dom";
 
 class Programs extends Component{
   render() {
-    let programs = this.props.programs;
-    console.log("Programs: " + programs);
-    console.log("všechny programy, které by měli pasovat k stanici"+this.props.match.params.tvchannel);
-    if(programs <= 0){
-      return  <p>Loading...</p>
-    } else {
-      console.log(programs);
-      let programFilter = programs.filter(
-      program => ( !(program.tvchannel === null) )  &&
-             (program.tvchannel.namePath === this.props.tvchannels)
-      );
-      if(programFilter.length <= 0){
-        return <p>Sorry there are no program on this channel</p>
-      } else {
-        return(
-          <div>
-            <Link to ={"/"}> <p>Home</p></Link>
-            <h1>Programs</h1>
-            <h3>List of programs</h3>
-            {programFilter.map(el => {
-              return <span><Link to={`/program/${el._id}`}>
-                     <p>{el.title}</p>
-                     </Link>{el.tvchannel === null ? 'tvchannel N/A' : el.tvchannel.name}</span>
-            })}      
-          </div>
-        )
-      }
-    }
+    return(
+
+    <div>
+      <h3>{this.props.match.tvchannel}</h3>
+      <h4>List of programs</h4>
+
+    </div>)
+    // let programs = this.props.programs;
+    // console.log("Programs: " + programs);
+    // console.log("všechny programy, které by měli pasovat k stanici"+this.props.match.params.tvchannel);
+    // if(programs <= 0){
+    //   return  <p>Loading...</p>
+    // } else {
+    //   console.log(programs);
+    //   let programFilter = programs.filter(
+    //   program => ( !(program.tvchannel === null) )  &&
+    //          (program.tvchannel.namePath === this.props.tvchannels)
+    //   );
+    //   if(programFilter.length <= 0){
+    //     return <p>Sorry there are no program on this channel</p>
+    //   } else {
+    //     return(
+    //       <div>
+    //         <Link to ={"/"}> <p>Home</p></Link>
+    //         <h1>Programs</h1>
+    //         <h3>List of programs</h3>
+    //         {programFilter.map(el => {
+    //           return <span><Link to={`/program/${el._id}`}>
+    //                  <p>{el.title}</p>
+    //                  </Link>{el.tvchannel === null ? 'tvchannel N/A' : el.tvchannel.name}</span>
+    //         })}
+    //       </div>
+    //     )
+    //   }
+    // }
   }
 }
 export default Programs;
