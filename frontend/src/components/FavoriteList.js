d1c934bb14c1eb62eb5059060ca21ea0a29d63d4
Albina Mrazova <mrazova.albina@gmail.com>
2019-08-07 08:49:58 +0200

    Adding favorite list 10

--- frontend/src/components/FavoriteList.js
+++ frontend/src/components/FavoriteList.js
@@ -3,23 +3,15 @@ import {Link} from "react-router-dom";
 
 export class Program extends Component {
     render() {
-        const favorite = this.props.users.favorite;
+
 
         console.log(this.props);
-        console.log(favorite);
-        if (!favorite) {
-            return <p>You don't have any favorite programs yet</p>
-        }
-        return (
+        return(
             <div>
-                <Link to ={"/"}> <p>Home</p></Link>
-                <header>
-                    <h1>Title : {favorite.title}</h1>
-                    <p>Next time in your tv: {favorite.next}</p>
-                    <p>Tv channel that it will be on: {favorite.tvchannel}</p>
-                </header>
+                <h1>Your movie schedule:</h1>
             </div>
         )
+
     }
 }
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -27,7 +27,6 @@ class Login extends Component{
       }).then(res => {
           if (res.status === 200) {
               console.log('hello world');
-              console.log(this.props);
               this.props.history.push(`/userSchedule`);
 
           }else{
