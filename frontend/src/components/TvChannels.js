bdbbc746e2f92ff5adf18f3fe6263bbe6855427a
Albina Mrazova <mrazova.albina@gmail.com>
2019-08-08 10:38:48 +0200

    sorting Fe2

--- frontend/src/components/TvChannels.js
+++ frontend/src/components/TvChannels.js
@@ -4,13 +4,13 @@ import {Link} from "react-router-dom";
 class TvChannels extends Component{
 
         render() {
-            console.log(this.props.tvchannel);
+            console.log(this.props.tvchannels);
             return(
             <div>
                 <Link to ={"/login"}> <p>Login</p></Link>
                 <h1>Tv Channels</h1>
 
-                {this.props.tvchannel.map(el => {
+                {this.props.tvchannels.map(el => {
                     return <Link key={el._id} to={`/programs/${el.namePath}`}><p>{el.name} <br></br>
                     </p></Link>
                 })}
